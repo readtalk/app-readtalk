2367875794ce63a6216937c9d248d331aae2a80b
READTalk <redtalk.app@hotmail.com>
2026-05-01 11:32:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://auth.readtalk.workers.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Agree & continue
         </a>
       </header>
     </div>
